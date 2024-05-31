b4bee056cae825d0b9781c7e263689907dde8d62
Y0un9Ki <srilankakim66@gmail.com>
2024-06-01 04:16:54 +0900

    UPDATED : deploy settings HOST ip change about front part

--- final/settings/prod.py
+++ final/settings/prod.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['34.47.95.16']
 
 
 # Application definition
