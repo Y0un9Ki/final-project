dba4f21fee13680e1037289c1cedc060902fc6be
Y0un9Ki <srilankakim66@gmail.com>
2024-06-02 20:19:56 +0900

    UPDATED : performancedetail API change about reserve certification part

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -92,16 +92,51 @@ class PerformanceCategoryDetail(APIView):
         return paginator.get_paginated_response(serializer.data)
         
 
-class PerformanceDetail(mixins.RetrieveModelMixin,
-                        generics.GenericAPIView):
-    serializer_class = PerformanceDetailSerializer
-    queryset = Performance.objects.all()
+class PerformanceDetail(APIView):
     permission_classes = [IsAuthenticated]
     # authentication_classes = [BasicAuthentication, SessionAuthentication]
     authentication_classes = [JWTAuthentication]
     
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    def get_object(self, pk, format=None):
+        try:
+            performance = Performance.objects.get(pk=pk)
+            return performance
+        except Performance.DoesNotExist:
+            raise NotFound({'message': '찾으시는 공연이 없어요'})
+    
+    def get(self, request, pk, format=None):
+        performance = self.get_object(pk)
+        user = self.request.user
+        serializer = PerformanceDetailSerializer(performance)
+        try:
+            Reservation.objects.get(user=user, performance=performance)
+            response_data = {
+                'id': serializer.data['id'],
+                'image': serializer.data['image'],
+                'name': serializer.data['name'],
+                'character': serializer.data['character'],
+                'price': serializer.data['price'],
+                'startdate': serializer.data['startdate'],
+                'venue': serializer.data['venue'],
+                'user': user.id,
+                'reserved': True,
+            }
+            return Response(response_data)
+        except Reservation.DoesNotExist:
+            response_data = {
+                'id': serializer.data['id'],
+                'image': serializer.data['image'],
+                'name': serializer.data['name'],
+                'character': serializer.data['character'],
+                'price': serializer.data['price'],
+                'startdate': serializer.data['startdate'],
+                'venue': serializer.data['venue'],
+                'user': user.id,
+                'reserved': False,
+            }
+            return Response(response_data)
+
+        
     
 class PerformanceModify(mixins.RetrieveModelMixin,
                         mixins.UpdateModelMixin,
@@ -188,43 +223,58 @@ class ReservationCreate(APIView):
     
     def post(self, request, pk, format=None):
         try:
-            performance = Performance.objects.get(pk=pk)
             user=self.request.user
+            performance = Performance.objects.get(pk=pk)
             user_point = user.point
+            print(performance)
             if user.point is None:
                 user.point = 0
                 user.save()
             performance_price = performance.price
-            print(request.data)
-            print(request)
-            request_data = {
-                'user': user.id,
-                'performance': pk
-            }
-            serializer = ReservationSerializer(data=request_data)
-            if serializer.is_valid():
-                if user_point >= performance_price:
-                    user.point = user_point - performance_price
-                    user.save()
-                    serializer.save(user=user, performance=performance)
+            # 이 try구문은 예약이 이미 존재한다면 다시 예약을 못하게 막아놓기 위해서 만들었다.
+            try:
+                reservation = Reservation.objects.get(performance=performance, user=user)
+                if reservation:
+                    response_data = {
+                        'message': '이미 예약이 존재해요'
+                    }
+                    return Response(response_data)
+            except Reservation.DoesNotExist:
+                # 영기야 POST 요청시에 reservation모델이 외래키로 참고하고 있는 (여기서는 reservation은 user랑 performance를 외래키로 참고 하고 있어) 값들의
+                # id값을(user와 performance의 id값) body에 꼭 넣어줘서 POST요청을 보내야만 생성이 되었다.
+                # 이전에도 이런 경험을 했었어!!!
+                # 근데 난 여기서 요청한 user를 식별할 수 있고, 엔드포인트에 performance의 id를
+                # 요청으로 받기에 결국에는 user의 id와 performance의 id를 알 수 있어 그래서 내가 자동으로 넣어주는 과정을 만들었다. 그러면 프론트에서 Post요청시에
+                # body에 아무런 값을 주지 않아도 내가 자동으로 넣어주기에 POST요청시에 reservation을 생성할 수 있어!!!!! 
+                # 아래에 코드야!!! 블로그 작성하자!!!
+                request_data = {
+                    'user': user.id,
+                    'performance': pk
+                }
+                serializer = ReservationSerializer(data=request_data)
+                if serializer.is_valid():
+                    if user_point >= performance_price:
+                        user.point = user_point - performance_price
+                        user.save()
+                        serializer.save(user=user, performance=performance)
+                        
+                        response_data = {
+                            'user_id': serializer.data['user'],
+                            'performance_id': serializer.data['performance'],
+                            'performance_name': performance.name,
+                            'message': '예약에 성공하셨습니다.'
+                        }
+                    else:
+                        point = performance_price - user_point
+                        response_data = {
+                            'user_id': user.id,
+                            'message': f'{point}포인트가 부족해요ㅠㅠ'
+                        }
                     
-                    response_data = {
-                        'user_id': serializer.data['user'],
-                        'performance_id': serializer.data['performance'],
-                        'performance_name': performance.name,
-                        'message': '예약에 성공하셨습니다.'
-                    }
+                    return Response(response_data, status=status.HTTP_200_OK)
                 else:
-                    point = performance_price - user_point
-                    response_data = {
-                        'user_id': user.id,
-                        'message': f'{point}포인트가 부족해요ㅠㅠ'
-                    }
-                
-                return Response(response_data, status=status.HTTP_200_OK)
-            else:
-                Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-                
+                    Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                    
         except Performance.DoesNotExist:
             raise NotFound({'message':'해당 공연은 존재하지 않습니다.'})
             
